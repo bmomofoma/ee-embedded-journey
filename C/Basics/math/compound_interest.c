325b5f6b24b3f237b7d5407c9044f548be6a4e57
Benedict-Michael Omofoma <mikeerune08@gmail.com>
2026-01-01 20:05:58 -0800

    Reorganize project structure and add compound interest calculator

---
+++ C/Basics/math/compound_interest.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    double principal = 0.0;
+    double rate = 0.0;
+    int years = 0;
+    int compounds_per_year = 0;
+    double amount = 0.0;
+
+    printf("Compound Interest Calculator\n");
+
+    printf("Enter the principal (P): ");
+    scanf("%lf", &principal);
+
+    printf("Enter the interest rate (r): ");
+    scanf("%lf", &rate);
+    rate /= 100;
+
+    printf("Enter the number of years (t): ");
+    scanf("%d", &years);
+
+    printf("Enter the number of times interest is compounded per year: ");
+    scanf("%d", &compounds_per_year);
+
+    amount = principal * pow(1 + rate / compounds_per_year, compounds_per_year * years);
+
+    printf("Over %d years, your total will become $%.2lf\n", years, amount);
+
+    return 0;
+}
